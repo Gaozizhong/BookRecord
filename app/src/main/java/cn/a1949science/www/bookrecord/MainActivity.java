5d1292b35d13010ff19d8f93a8be4a9f68837f1c
Gaozizhong <631923319@qq.com>
2017-12-26 14:37:31 +0800

    first commit

---
+++ app/src/main/java/cn/a1949science/www/bookrecord/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.a1949science.www.bookrecord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
