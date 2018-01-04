fa21dc2cc48f0d3dfaa677e51c13d35cea12cd89
Gaozizhong <631923319@qq.com>
2018-01-04 17:22:52 +0800

    手机号验证

--- app/src/main/java/cn/a1949science/www/bookrecord/activity/LoginActivity.java
+++ app/src/main/java/cn/a1949science/www/bookrecord/activity/LoginActivity.java
@@ -1,5 +1,6 @@
 package cn.a1949science.www.bookrecord.activity;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -8,18 +9,23 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import cn.a1949science.www.bookrecord.R;
+import cn.a1949science.www.bookrecord.utils.AMUtils;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Context mContext = LoginActivity.this;
+    EditText phoneNumber,verification;
+    Button delete,getverification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        final EditText phoneNumber=(EditText)findViewById(R.id.phoneNumber);
+        phoneNumber=(EditText)findViewById(R.id.phoneNumber);
 
-        final EditText verification=(EditText)findViewById(R.id.verification);
-        Button delete=(Button)findViewById(R.id.phoneNumberDelete);
-        Button getverification=(Button)findViewById(R.id.VerificationButton);
+        verification=(EditText)findViewById(R.id.verification);
+        delete=(Button)findViewById(R.id.phoneNumberDelete);
+        getverification=(Button)findViewById(R.id.VerificationButton);
         //对输入的电话号码进行判断
         phoneNumber.addTextChangedListener(new TextWatcher() {
             @Override
@@ -28,15 +34,11 @@ public class LoginActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                String temp=s.toString();
-                if(temp.length()>11)
-                {
-                    phoneNumber.setText(s);
-
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.length() == 11) {
+                    AMUtils.onInactive(mContext, phoneNumber);
                 }
             }
-
             @Override
             public void afterTextChanged(Editable s) {
 
