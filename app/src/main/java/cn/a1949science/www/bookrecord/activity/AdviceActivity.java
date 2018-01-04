4013b0cc707dda4123e63ccef981ce66b6f79d09
Gaozizhong <631923319@qq.com>
2018-01-04 19:48:46 +0800

    页面增加

---
+++ app/src/main/java/cn/a1949science/www/bookrecord/activity/AdviceActivity.java
@@ -0,0 +1,15 @@
+package cn.a1949science.www.bookrecord.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.a1949science.www.bookrecord.R;
+
+public class AdviceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advice);
+    }
+}
