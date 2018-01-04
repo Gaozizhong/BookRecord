aa669d526080e9d5a8a815d3e8fd1aaaab1034a7
liuxiaotong <1074718775@qq.com>
2018-01-04 17:01:42 +0800

    登录界面第2次提交

--- app/src/main/java/cn/a1949science/www/bookrecord/LoginActivity.java
+++ app/src/main/java/cn/a1949science/www/bookrecord/LoginActivity.java
@@ -2,6 +2,10 @@ package cn.a1949science.www.bookrecord;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,6 +13,36 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        final EditText phoneNumber=(EditText)findViewById(R.id.phoneNumber);
 
+        final EditText verification=(EditText)findViewById(R.id.verification);
+        Button delete=(Button)findViewById(R.id.phoneNumberDelete);
+        Button getverification=(Button)findViewById(R.id.VerificationButton);
+        //对输入的电话号码进行判断
+        phoneNumber.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String temp=s.toString();
+                if(temp.length()>11)
+                {
+                    phoneNumber.setText(s);
+
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+            public void ifphoneNumber() {
+
+
+            }
+        });
     }
 }
