3ef6fb860e0646657269bb1530b52734154eb51e
Gaozizhong <631923319@qq.com>
2018-01-07 14:10:42 +0800

    首页侧滑栏完成

--- app/src/main/java/cn/a1949science/www/bookrecord/activity/MainActivity.java
+++ app/src/main/java/cn/a1949science/www/bookrecord/activity/MainActivity.java
@@ -1,15 +1,121 @@
 package cn.a1949science.www.bookrecord.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import cn.a1949science.www.bookrecord.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    Context mContext = MainActivity.this;
+    SwipeRefreshLayout refresh;
+    RecyclerView recyclerView;
+    private long exitTime = 0;
+    Toolbar toolbar;
+    DrawerLayout drawer;
+    View headerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        headerLayout = navigationView.getHeaderView(0);
+        headerLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent it = new Intent(mContext,User_Info.class);
+                //startActivity(it);
+                //overridePendingTransition(R.anim.slide_right_in,R.anim.slide_left_out);
+            }
+        });
+    }
+
+    private void initView() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        //下拉刷新
+        refresh = findViewById(R.id.refresh);
+        recyclerView = findViewById(R.id.list);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+        addDate();
+        //adapter = new myAdapterRecyclerView(mContext, routeInfos);
+        //recyclerView.setAdapter(adapter);
+        refresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+            }
+        });
+    }
+
+    private void addDate() {
+        Toast.makeText(mContext, "请加载数据", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        }//点击两次返回键退出程序
+        else if(System.currentTimeMillis() - exitTime > 2000) {
+            Toast.makeText(this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
+            exitTime = System.currentTimeMillis();
+        } else {
+            finish();
+            System.exit(0);
+            android.os.Process.killProcess(android.os.Process.myPid());
+        }
+    }
+
+    //侧滑菜单中的点击事件
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_statistic) {
+            // Handle the camera action
+        } else if (id == R.id.nav_advice) {
+
+        } else if (id == R.id.nav_about) {
+
+        } else if (id == R.id.nav_update) {
+
+        }else if (id == R.id.nav_quit) {
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
