5df20d2138ffd26617a655ac803f20533d1d0e29
Gaozizhong <631923319@qq.com>
2018-06-19 09:44:30 +0800

    豆瓣搜索还没显示好，增加数据库操作类

---
+++ app/src/main/java/cn/a1949science/www/bookrecord/database/QueryReading.java
@@ -0,0 +1,32 @@
+package cn.a1949science.www.bookrecord.database;
+
+import cn.a1949science.www.bookrecord.bean.ReadInfo;
+import cn.a1949science.www.bookrecord.bean._User;
+
+/**
+ * 对Reading列表进行操作
+ * 数据库操作类
+ * Created by 高子忠 on 2018/6/19.
+ */
+
+public class QueryReading {
+    /**
+     * 查询ReadInfo表中是否存在这条信息
+     * 数据库操作类
+     * 返回值：Info对象
+     */
+    public static ReadInfo queryBookInfo(_User user, String book_isbn) {
+
+        return null;
+    }
+
+    /**
+     * 对ReadInfo表中存在某条信息进行更改
+     * 数据库操作类
+     * 返回值：Info对象
+     */
+    public static ReadInfo parsingBookInfo(_User user, String book_isbn) {
+
+        return null;
+    }
+}
---
+++ app/src/main/java/cn/a1949science/www/bookrecord/database/QueryWantRead.java
@@ -0,0 +1,43 @@
+package cn.a1949science.www.bookrecord.database;
+
+import cn.a1949science.www.bookrecord.bean.ReadInfo;
+import cn.a1949science.www.bookrecord.bean._User;
+
+/**
+ * 对WantRead列表进行操作
+ * 数据库操作类
+ * Created by 高子忠 on 2018/6/19.
+ */
+
+public class QueryWantRead {
+    /**
+     * 查询ReadInfo表中是否存在这条信息
+     * 数据库操作类
+     * 返回值：Info对象
+     */
+    public static ReadInfo queryBookInfo(_User user, String book_isbn) {
+
+        return null;
+    }
+
+    /**
+     * 对ReadInfo表中存在某条信息进行更改
+     * 数据库操作类
+     * 返回值：ReadInfo对象
+     */
+    public static ReadInfo updateBookInfo(ReadInfo bookInfo, String book_isbn) {
+
+        return null;
+    }
+
+    /**
+     * 对ReadInfo表中存在某条信息进行删除
+     * 数据库操作类
+     * 返回值：Info对象
+     */
+    public static ReadInfo deleteBookInfo(ReadInfo bookInfo, String book_isbn) {
+
+        return null;
+    }
+
+}
